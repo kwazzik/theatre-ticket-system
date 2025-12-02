754c44170e014280d5cc76b621f11f015d6d2ca3
chtot <mishakwazzi@gmail.com>
2025-12-02 19:16:43 +0200

    update string in settings.py

--- theatre/settings.py
+++ theatre/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "drf-spectacular",
+    "drf_spectacular",
+    "debug_toolbar",
     "app",
     "user",
 ]
